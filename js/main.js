9d72339a5cb10a95486dc4348f45f9185351f4d0
David Broderick <David.Broderick@saratos.com>
2013-02-04 20:53:47 +0000

    4
    4

--- js/main.js
+++ js/main.js
@@ -22,6 +22,18 @@ var app = {
         }
     },
 
+    renderHomeView: function() {
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
+    findByName: function() {
+        var self = this;
+        this.store.findByName($('.search-key').val(), function(employees) {
+            $('.employee-list').html(self.employeeLiTpl(employees));
+        });
+    },
+
     initialize: function() {
 
         var self = this;
@@ -29,6 +41,9 @@ var app = {
             self.showAlert('Store Initialized', 'Info');
         });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
 
     }
 
