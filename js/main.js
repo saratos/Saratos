fb0b7a176078d1f113955da6acde7118d81856a1
David Broderick <David.Broderick@saratos.com>
2013-02-10 17:45:24 +0000

    s8
    s8

--- js/main.js
+++ js/main.js
@@ -4,7 +4,25 @@ var app = {
 
     initialize: function () {
 
-        this.longstring = "<H2>List of courses from the Web</H2>"
+        self.renderHomeView();
+    },
+
+    displayNow: function () {
+        var currentDate = new Date()
+        var day = currentDate.getDate()
+        var month = currentDate.getMonth() + 1
+        var year = currentDate.getFullYear()
+        this.currentNow = ("<b>" + day + "/" + month + "/" + year + "</b>")
+    },
+
+    renderHomeView: function() {
+        var html =
+            "<div class='header'><h1>Home</h1></div>" +
+                "<div class='search-view'>" +
+                "<input class='search-key'/>" +
+                "<ul class='employee-list'></ul>" +
+                "</div>"
+        $('body').html(html);
     }
 }
 
